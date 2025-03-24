ae38f915d7f07e1fa0a37ff48ac6865b5606c306
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-03-24 17:06:37 +0100

    Commit global

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './styles/index.css';
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Error from "./pages/Error";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
             </Routes>
         </Router>
     </React.StrictMode>
